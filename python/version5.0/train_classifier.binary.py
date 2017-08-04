680dd9c412d083e48469e2132e34ce8541078894
Rachelmarty20 <Rachelmarty20@gmail.com>
2017-08-04 13:50:55 +0200

    updating paths

--- python/version5.0/train_classifier.binary.py
+++ python/version5.0/train_classifier.binary.py
@@ -13,12 +13,12 @@ from sklearn.externals import joblib
 
 def main(species, image_size):
 
-    larvae_files = os.listdir('/cellar/users/ramarty/Data/ants/gold_standard/training/{0}/size_{1}/larvae'.format(species, image_size))
-    pupae_files = os.listdir('/cellar/users/ramarty/Data/ants/gold_standard/training/{0}/size_{1}/pupae'.format(species, image_size))
-    other_files = os.listdir('/cellar/users/ramarty/Data/ants/gold_standard/training/{0}/size_{1}/other'.format(species, image_size))
+    larvae_files = os.listdir('/cellar/users/ramarty/Data/ants/version2.0/training/{0}/size_{1}/larvae'.format(species, image_size))
+    pupae_files = os.listdir('/cellar/users/ramarty/Data/ants/version2.0/training/{0}/size_{1}/pupae'.format(species, image_size))
+    other_files = os.listdir('/cellar/users/ramarty/Data/ants/version2.0/training/{0}/size_{1}/other'.format(species, image_size))
 
-    larvae_images = [mh.imread('/cellar/users/ramarty/Data/ants/gold_standard/training/{0}/size_{1}/larvae/{2}'.format(species, image_size, x)) for x in larvae_files] + [mh.imread('/cellar/users/ramarty/Data/ants/gold_standard/training/{0}/size_{1}/pupae/{2}'.format(species, image_size, x)) for x in pupae_files]
-    other_images = [mh.imread('/cellar/users/ramarty/Data/ants/gold_standard/training/{0}/size_{1}/other/{2}'.format(species, image_size, x)) for x in other_files]
+    larvae_images = [mh.imread('/cellar/users/ramarty/Data/ants/version2.0/training/{0}/size_{1}/larvae/{2}'.format(species, image_size, x)) for x in larvae_files] + [mh.imread('/cellar/users/ramarty/Data/ants/version2.0/training/{0}/size_{1}/pupae/{2}'.format(species, image_size, x)) for x in pupae_files]
+    other_images = [mh.imread('/cellar/users/ramarty/Data/ants/version2.0/training/{0}/size_{1}/other/{2}'.format(species, image_size, x)) for x in other_files]
 
     images = larvae_images + other_images
     classifications = ['larvae' for x in larvae_images] + ['other' for x in other_images]
@@ -31,12 +31,12 @@ def main(species, image_size):
     predictions = cross_validation.cross_val_predict(clf, X_train, y_train)
     acc = np.mean(predictions == y_train)
 
-    with open('/cellar/users/ramarty/Data/ants/gold_standard/classifiers/{0}/CV_accuracy.random_forest.size_{1}.txt'.format(species, image_size) , 'w') as outfile:
+    with open('/cellar/users/ramarty/Data/ants/version5.0/classifiers/{0}/CV_accuracy.random_forest.{1}.txt'.format(species, image_size) , 'w') as outfile:
         outfile.write("Accuracy: {:.2%}".format(acc))
 
     clf.fit(X_train, y_train)
 
-    joblib.dump(clf, '/cellar/users/ramarty/Data/ants/gold_standard/classifiers/{0}/random_forest.size_{1}.pkl'.format(species, image_size), compress=9)
+    joblib.dump(clf, '/cellar/users/ramarty/Data/ants/version5.0/classifiers/{0}/random_forest.{1}.pkl'.format(species, image_size), compress=9)
 
 
 
