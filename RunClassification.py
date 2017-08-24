34982a56c0f1a4cace66004aec10b078c9aba241
Rachelmarty20 <Rachelmarty20@gmail.com>
2017-08-24 10:47:03 +0200

    removing old references

--- RunClassification.py
+++ RunClassification.py
@@ -9,7 +9,7 @@ __version__ = "0.1.0"
 __license__ = "UCSD"
 
 import argparse
-import developement.classification as classification
+import developement.claication as claication
 from sklearn.externals import joblib
 import cPickle as pickle
 import warnings
@@ -17,41 +17,39 @@ warnings.filterwarnings('ignore')
 
 def main(args):
 
-    print(args.classify, args.assess)
-
     # Upload image
-    id = classification.Classification(image_path=args.image_path, square_size=args.square_size,
+    cla = claication.claication(image_path=args.image_path, square_size=args.square_size,
                                        filter1=args.filter1, kernel_size=args.kernel_size,
                                        filter2=args.filter2, sample_name=args.sample_name)
-    id.get_image_matrix()
+    cla.get_image_matrix()
     print("Image loaded.")
 
-    # Import classifier
-    model = joblib.load(args.classifier_path)
+    # Import claier
+    model = joblib.load(args.claier_path)
     print("Model loaded.")
 
-    # Run classification
+    # Run claication
     print("Predicting...")
-    id.predict(model)
-    id.summarize_predictions()
+    cla.predict(model)
+    cla.summarize_predictions()
     print("Predictions made.")
 
     # Perform filtering
     print("Filtering...")
-    id.filter()
+    cla.filter()
     print("Filtering done.")
 
     # Save image and data file
-    id.save_results(args.output_direcotry)
+    cla.save_results(args.output_direcotry)
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser("This program is used to identify the position of brood in an image.")
+    parser = argparse.ArgumentParser("This program is used to claentify the position of brood in an image.")
 
     # required arguements
-    parser.add_argument("classifier_path", help="Path to model")
-    parser.add_argument("image_path", help="Path to image to classify")
+    parser.add_argument("claier_path", help="Path to model")
+    parser.add_argument("image_path", help="Path to image to clay (pgm)")
     parser.add_argument("output_directory", help="Path for results directory")
 
     # optional arguments
